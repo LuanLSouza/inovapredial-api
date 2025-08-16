e17e3e295d7810ea199c56fe5b10089a1c67bf47
LuanLSouza <luanlimas328@gmail.com>
2025-08-15 22:29:47 -0300

    inital commit

---
+++ src/main/java/com/inovapredial/InovapredialApplication.java
@@ -0,0 +1,13 @@
+package com.inovapredial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InovapredialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InovapredialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inovapredial/InovapredialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inovapredial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InovapredialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
