16663c181507210ffdae76b6fa761e355f75374b
LuanLSouza <luanlimas328@gmail.com>
2025-09-01 11:54:25 -0300

    Fix DTO

--- src/main/java/com/inovapredial/dto/BuildingResponseDTO.java
+++ src/main/java/com/inovapredial/dto/BuildingResponseDTO.java
@@ -1,13 +1,12 @@
 package com.inovapredial.dto;
 
 import com.inovapredial.model.enums.BuildingType;
-import jakarta.validation.constraints.NotBlank;
 
 import java.util.UUID;
 
 public record BuildingResponseDTO(
         UUID id,
-        @NotBlank(message = "Campo obrigatório") String name,
+        String name,
         BuildingType buildingType,
         Integer constructionYear,
         String description,
--- src/main/java/com/inovapredial/mapper/BuildingMapper.java
+++ src/main/java/com/inovapredial/mapper/BuildingMapper.java
@@ -10,8 +10,6 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
-import java.util.List;
-
 @Mapper(componentModel = "spring")
 public interface BuildingMapper {
 
