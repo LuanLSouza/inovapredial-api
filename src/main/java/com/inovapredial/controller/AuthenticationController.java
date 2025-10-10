7531471feaf07d9a0f7cbb2f9c0302e6bd7d541a
LuanLSouza <luanlimas328@gmail.com>
2025-10-09 22:52:00 -0300

    Remove @Hidden annotation from authentication and user controllers

--- src/main/java/com/inovapredial/controller/AuthenticationController.java
+++ src/main/java/com/inovapredial/controller/AuthenticationController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-@Hidden
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("auth")
